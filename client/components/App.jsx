ae502f02240f6b59c60930757fae375f36b8fda0
Tayla Dunlop <103092915+tayla-dunlop@users.noreply.github.com>
2022-06-24 10:32:04 +1200

    start api function

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -1,21 +1,16 @@
 import React, { useState, useEffect } from 'react'
 
-import { getWelcome } from '../apiClient'
+import { getRandomDog } from '../apiClient'
 
 function App() {
-  const [welcomeStatement, setWelcomeStatement] = useState('')
 
-  useEffect(() => {
-    getWelcome()
-      .then((res) => {
-        setWelcomeStatement(res.statement)
-      })
-      .catch((err) => {
-        console.error(err.message)
-      })
-  })
+  return (
+    <>
+      <h1>Dogs!</h1>
+      <button onClick={getRandomDog}>Get Dog</button>
+    </>
+)
 
-  return <h1>{welcomeStatement}</h1>
 }
 
 export default App
